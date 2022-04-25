ba68f0ba2dac17827bd71f919515feb2d9b0ad90
jake.0208(조재덕)/kakao <jake.0208@kakaocorp.com>
2022-04-25 10:35:01 +0900

    FEAT: ToDo List 구현

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
+import {useState} from 'react'
+
 function App() {
+    const [toDo, setToDo] = useState("")
+    const [toDos, setTodos] = useState([])
+    const onChange = (event) => setToDo(event.target.value);
+    const onSubmit = (event) => {
+        event.preventDefault();
+        if (toDo === "") {
+            return;
+        }
+        setTodos(currentArray => [toDo, ...currentArray]);
+        setToDo("");
+    }
     return (
         <div>
-            <h1>Welcome back to React!</h1>
+            <h1>My ToDos ({toDos.length})</h1>
+            <form onSubmit={onSubmit}>
+                <input onChange={onChange}
+                       type="text"
+                       value={toDo}
+                       placeholder="Write your to do..."/>
+                <button>Add To Do</button>
+            </form>
+            <hr/>
+            {toDos.map((item,index) => <li key={index}>{item}</li>)}
         </div>
     );
 }
